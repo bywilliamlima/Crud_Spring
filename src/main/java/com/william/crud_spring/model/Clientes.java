e9bcb111205180f29c398a2c760f247a3b68f0a1
bywilliamlima <williamlimavg@gmail.com>
2024-08-21 16:52:49 -0300

    date

--- src/main/java/com/william/crud_spring/model/Clientes.java
+++ src/main/java/com/william/crud_spring/model/Clientes.java
@@ -1,5 +1,7 @@
 package com.william.crud_spring.model;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,29 +9,35 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import lombok.Data;
 
 @Data
-@Entity 
-public class Clientes {
+@Entity
+public class Clientes {  // Mudança do nome da classe para o singular
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty("_id")
     private Long id;
-    
-  
+
     @Column(length = 200, nullable = false)
     private String name;
 
-    @Column(length= 20, nullable = false)
+    @Column(length = 20, nullable = false)
     private String category;
 
-    @Column(length= 20, nullable = false)
+    @Column(length = 20, nullable = false)
     private String cnpj;
 
-    @Column(length= 20, nullable = false)
+    @Column(length = 20, nullable = false)
     private String phone;
 
-    
+    @Column(nullable = false)
+    private LocalDateTime dataCadastro;
+
+    @PrePersist
+    protected void onCreate() {
+        this.dataCadastro = LocalDateTime.now();
+    }
 }
